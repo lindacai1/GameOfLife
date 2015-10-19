923c9b868491ae3c8034cf22ac5fe9de5a676a27
Linda Cai <lindakcai@gmail.com>
2015-10-18 21:06:05 -0700

    commit

--- GameOfLife/GameOfLife/disk_board.cpp
+++ GameOfLife/GameOfLife/disk_board.cpp
@@ -1,4 +1,6 @@
 #include "disk_board.h"
+#include "binary_board_writer.h"
+#include <limits>
 using std::unique_ptr;
 using std::istream;
 using std::iostream;
@@ -17,24 +19,32 @@ DiskBoard::DiskBoard(DiskBoard&& x) : livecells(std::move(x.livecells)) {
  * Algorithm:
  */
 DiskBoard DiskBoard::nextIteration(unique_ptr<iostream> output) {
-	std::vector<ColumnBuffer> buffers;
+	//std::vector<ColumnBuffer> buffers;
 
-	int64_t pos = 0;
+	//int64_t pos = 0;
 
-	// Make 5 column buffers with <= 5 y-values using data stream
-	for (int i = 0; i < 5; ++i) {
-		ColumnBuffer cb;
-		if (!readBuffer(cb, pos)) {
-			break;
-		}
+	///* Read in a 5x5 area at a time
+	// * Make 5 column buffers with <= 5 y-values using data stream 
+	// */
+	//for (int i = 0; i < 5; ++i) {
+	//	ColumnBuffer cb;
+	//	if (!readBuffer(cb, pos)) {
+	//		break;
+	//	}
 
-		buffers.push_back(cb);
-		pos = cb.getNextStartPos();
-	}
+	//	buffers.push_back(cb);
+	//	pos = cb.getNextStartPos();
+	//}
 
+	//int64_t currentCol = buffers.front().x;
 
+	//// Get the minimum possible x value of the next column to check
+	//int64_t mNextCol = std::numeric_limits<int64_t>::min();
 
+	BinaryBoardWriter writer(*output);
+	BinaryBoardReader reader(*livecells);
 
+	ColumnIterator cIter(*livecells);
 
 
 	return DiskBoard(std::unique_ptr<istream>());
@@ -69,6 +79,91 @@ bool DiskBoard::readBuffer(ColumnBuffer& cb, int64_t pos) {
 
 	return true;
 }
+
+struct ColumnInfo {
+	int64_t x;
+	int64_t startPos;
+	int64_t len;
+	bool isValid; // True if this is a valid column
+
+	ColumnInfo() : x(0), startPos(0), len(0), isValid(false) {
+	}
+
+	int64_t getNextPos() {
+		return startPos + (sizeof(int64_t)) * (2 + len);
+	}
+
+	void setFromStream(istream& input, int64_t pos) {
+		input.seekg(pos);
+		startPos = pos;
+		input.read(reinterpret_cast<char*> (&x), sizeof(x));
+		if (input.eof()) {
+			isValid = false;
+			return;
+		}
+		input.read(reinterpret_cast<char*> (&len), sizeof(len));
+		if (input.eof()) {
+			isValid = false;
+			return;
+		}
+		isValid = true;
+	}
+};
+
+class ColumnIterator {
+	ColumnInfo ci;
+	istream& input;
+	int64_t minNextColToCheck = std::numeric_limits<int64_t>::min();
+	bool sawMaxNum = false;
+	
+	ColumnIterator(istream& input) : input(input) {
+		ci.setFromStream(input, 0);
+	}
+
+	// TODO: Handle overflow
+	bool getNextColToCheck(int64_t& outCol) {
+		while (ci.isValid) {
+			if (sawMaxNum) {
+				return false;
+			}
+			// Check column to the left
+			if (ci.x > std::numeric_limits<int64_t>::min()) {
+				if (ci.x - 1 >= minNextColToCheck) {
+					minNextColToCheck = ci.x;
+					outCol = ci.x - 1;
+					return true;
+				}
+			}
+			// Check current column
+			if (ci.x >= minNextColToCheck) {
+				if (ci.x < std::numeric_limits<int64_t>::max()) {
+					minNextColToCheck = ci.x + 1;
+				} else {
+					minNextColToCheck = ci.x;
+					sawMaxNum = true;
+				}
+				outCol = ci.x;
+				return true;
+			}
+			// Check column to the right
+			if (ci.x < std::numeric_limits<int64_t>::max()) {
+				if (ci.x == std::numeric_limits<int64_t>::max() - 1) {
+					sawMaxNum = true;
+					minNextColToCheck = ci.x + 1; // +2 would overflow
+					outCol = ci.x + 1;
+					return true;
+				}
+				if (ci.x + 1 >= minNextColToCheck) {
+					minNextColToCheck = ci.x + 2;
+					outCol = ci.x + 1;
+					return true;
+				}
+			}
+			ci.setFromStream(input, ci.getNextPos());
+		}
+		return false;
+	}
+};
 
 DiskBoard::ColumnBuffer::ColumnBuffer() : x(0), startPos(0), len(0), yPos(0) {
 }
--- GameOfLife/GameOfLife/disk_board.h
+++ GameOfLife/GameOfLife/disk_board.h
@@ -22,4 +22,24 @@ private:
 		ColumnBuffer();
 		int64_t getNextStartPos();
 	};
+
+	class ColumnIterator {
+		ColumnInfo ci;
+		std::istream& input;
+		int64_t minNextColToCheck;
+		bool sawMaxNum;
+		ColumnIterator(std::istream& input);
+		bool getNextColToCheck(int64_t& outCol);
+	};
+
+	struct ColumnInfo {
+		int64_t x;
+		int64_t startPos;
+		int64_t len;
+		bool isValid; // True if this is a valid column
+		ColumnInfo();
+		int64_t getNextPos();
+		void setFromStream(istream& input, int64_t pos);
+	};
+
 };
