31b8222eabdb9fc57de5bb67d19bf07df134121d
Linda Cai <lindakcai@gmail.com>
2015-10-18 18:01:23 -0700

    updated DiskBoard

--- GameOfLife/GameOfLife/disk_board.cpp
+++ GameOfLife/GameOfLife/disk_board.cpp
@@ -8,9 +8,71 @@ using std::iostream;
 DiskBoard::DiskBoard(unique_ptr<istream> data) : livecells(std::move(data)) {
 }
 
+// Need to specify explicitly because VS2013 doesn't provide default move constructor
+DiskBoard::DiskBoard(DiskBoard&& x) : livecells(std::move(x.livecells)) {
+}
+
 /* Our data (livecells) is a sorted binary file of the format: x, length of y values, y values
- * Stream in a small number of values of 3 adjacent rows
+ * 
+ * Algorithm:
  */
 DiskBoard DiskBoard::nextIteration(unique_ptr<iostream> output) {
+	std::vector<ColumnBuffer> buffers;
+
+	int64_t pos = 0;
+
+	// Make 5 column buffers with <= 5 y-values using data stream
+	for (int i = 0; i < 5; ++i) {
+		ColumnBuffer cb;
+		if (!readBuffer(cb, pos)) {
+			break;
+		}
+
+		buffers.push_back(cb);
+		pos = cb.getNextStartPos();
+	}
+
+
+
+
+
+
+	return DiskBoard(std::unique_ptr<istream>());
+}
 	
+bool DiskBoard::readBuffer(ColumnBuffer& cb, int64_t pos) {
+	livecells->seekg(pos);
+	cb.startPos = pos;
+	int64_t x;
+	livecells->read(reinterpret_cast<char*> (&x), sizeof(x));
+	if (livecells->eof()) {
+		return false;
+	}
+	cb.x = x;
+
+	int64_t len;
+	livecells->read(reinterpret_cast<char*> (&len), sizeof(len));
+	if (livecells->eof()) {
+		return false;
+	}
+	cb.len = len;
+
+	cb.yPos = livecells->tellg();
+	for (int i = 0; i < 5 && i < len; ++i) {
+		int64_t y;
+		livecells->read(reinterpret_cast<char*> (&y), sizeof(y));
+		if (livecells->eof()) {
+			return false;
+		}
+		cb.bufferedVals.push_back(y);
+	}
+
+	return true;
+}
+
+DiskBoard::ColumnBuffer::ColumnBuffer() : x(0), startPos(0), len(0), yPos(0) {
+}
+
+int64_t DiskBoard::ColumnBuffer::getNextStartPos() {
+	return startPos + (sizeof(int64_t)) * (2 + len);
 }
--- GameOfLife/GameOfLife/disk_board.h
+++ GameOfLife/GameOfLife/disk_board.h
@@ -1,8 +1,25 @@
 #include <istream>
 #include <memory>
+#include <vector>
 
 class DiskBoard {
+	class ColumnBuffer;
+public:
 	DiskBoard(std::unique_ptr<std::istream> data);
-	std::unique_ptr<std::istream> livecells;
 	DiskBoard nextIteration(std::unique_ptr<std::iostream> output);
+	DiskBoard(DiskBoard&&);
+private:
+	std::unique_ptr<std::istream> livecells;
+	bool readBuffer(ColumnBuffer& cb, int64_t pos);
+	
+	class ColumnBuffer {
+	public:
+		int64_t x;
+		int64_t startPos; // start of column; is the position of the x-value
+		int64_t len;
+		int64_t yPos; // start position of current set of y-values
+		std::vector<int64_t> bufferedVals;
+		ColumnBuffer();
+		int64_t getNextStartPos();
+	};
 };
